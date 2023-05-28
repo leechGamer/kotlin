2a9a7ca8db8c4175af47ee29ee1d0eb328899de1
g <ruddmsdl000@naver.com>
2023-05-28 17:50:22 +0900

    4.1.4 바깥쪽 class는 nested class의 private 멤버에 접근 불가

--- 4/Person.kt
+++ 4/Person.kt
@@ -1,10 +1,6 @@
-class Person (val id: Id, val age: Int) {
-    class Id (val firstName: String, val familyName: String)
-    fun showMe() = println("${id.firstName} ${id.familyName}, $age")
-}
-
-fun main() {
-    val id = Person.Id("john", "caporicci")
-    val person = Person(id, 25)
-    person.showMe() // john caporicci, 25
+class Person (private val id: Id, private val age: Int) {
+    class Id (private val firstName: String, private val familyName: String) {
+        fun nameSake(person: Person) = person.id.firstName == firstName
+    }
+    fun showMe() = println("${id.firstName} ${id.familyName}, $age") // error: 바깥쪽 class는 nested class의 private 멤버에 접근 불가
 }

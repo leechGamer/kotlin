f8c232c294aa322136917199356348e4ea2e0d62
g <ruddmsdl000@naver.com>
2023-05-28 15:30:28 +0900

    4.1.2 초기화블록에서는 return을 허용하지 않는다

--- 4/Person.kt
+++ 4/Person.kt
@@ -2,6 +2,7 @@ class Person(firstName: String, familyName: String) {
     var fullName = "$firstName $familyName"
 
     init {
+        if (firstName.isEmpty() && familyName.isEmpty()) return  // 초기화 블록에서는 return 을 허용하지 않는다.
         println("created new Person instance: $fullName")
     }
 }

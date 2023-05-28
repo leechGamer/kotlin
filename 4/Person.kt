1abd4ed0b8db117f730c64d1c6c01cc19597832b
g <ruddmsdl000@naver.com>
2023-05-28 16:06:42 +0900

    4.1.2 생성자 파라미터의 값을 멤버 프로퍼티로 만들기

--- 4/Person.kt
+++ 4/Person.kt
@@ -1,8 +1,7 @@
 
-class Person(firstName: String, familyName: String) {
-    val firstName = firstName // 주생성자 파라미터를 초기화나 init 밖에서 사용하려면, 생성자 파라미터 값을 저장할 property를 정의하자
-    val fullName = "$firstName $familyName"
-    fun printFirstName() {
-        println(firstName)
+class Person(val firstName: String, familyName: String) {
+        val fullName = "$firstName $familyName"
+        fun printFirstName() {
+            println(firstName)
+        }
     }
-}

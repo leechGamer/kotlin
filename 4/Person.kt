d431d2d985d1eb7b41179f05f01dff6b34a0dffa
g <ruddmsdl000@naver.com>
2023-05-28 17:20:54 +0900

    클래스에 주생성자가 있다면, (부생성자가 있는 경우) 모든 부생성자는 주생성자에 위임을 해야한다

--- 4/Person.kt
+++ 4/Person.kt
@@ -1,9 +1,5 @@
-class Person {
-    val fullName: String
-
-    constructor(firstName: String, familyName: String): // : (콜론)을 넣고 그 뒤에 일반 함수를 호출하는 거처럼 코드를 작성하되 함수 이름 대신 this를 사용하면 생성자 위임 호출이 된다 
+class Person (val fullName: String) {
+    constructor(firstName: String, familyName: String):
             this("$firstName $familyName")
-    constructor(fullName: String) {
-        this.fullName = fullName
-    }
 }
+

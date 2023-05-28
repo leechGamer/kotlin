e2feb211946ddeee45b86a55c1b2459751a1cca0
g <ruddmsdl000@naver.com>
2023-05-28 18:30:17 +0900

    4.1.5 지역클래스 예제

--- 4/Person.kt
+++ 4/Person.kt
@@ -1,19 +1,10 @@
-class Person (private val firstName: String, private val familyName: String) {
-    inner class Possession(val description: String) {
-        fun showOwner() = println(fullName())
-    }
-    private fun fullName() = "$firstName $familyName"
-}
-
 fun main() {
-   class Point(val x: Int, val y: Int) {
-       fun shift(dx: Int, dy: Int): Point = Point(x + dx, dy)
-       override fun toString() = "($x, $y)"
-   }
-    val p = Point(10, 10)
-    println(p.shift(-1, 3))
-}
-
-fun foo() {
-    println(Point(0,0)) // error : unresolved reference : Point
+  var x = 1
+    class Counter {
+        fun increment() {
+            x++;
+        }
+    }
+    Counter().increment()
+    println(x)
 }

e8ea4f00181774893bf8d583f4e65d938666f9f6
g <ruddmsdl000@naver.com>
2023-05-28 17:41:37 +0900

    4.1.4 nested class 예제

--- 4/Person.kt
+++ 4/Person.kt
@@ -1,5 +1,10 @@
-class Person (val fullName: String) {
-    constructor(firstName: String, familyName: String):
-            this("$firstName $familyName")
+class Person (val id: Id, val age: Int) {
+    class Id (val firstName: String, val familyName: String)
+    fun showMe() = println("${id.firstName} ${id.familyName}, $age")
 }
 
+fun main() {
+    val id = Person.Id("john", "caporicci")
+    val person = Person(id, 25)
+    person.showMe() // john caporicci, 25
+}

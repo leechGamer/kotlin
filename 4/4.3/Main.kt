6a84880e5c0b7a4a1fe668d3a0cd1a8f3e318f62
g <ruddmsdl000@naver.com>
2023-05-29 21:48:00 +0900

    4.3.3 custom getter (property와 getter정의에서 property타입 생략가능하고 타입추론에 의존 from 코틀린 1.1)

--- 4/4.3/Main.kt
+++ 4/4.3/Main.kt
@@ -1,7 +1,5 @@
 class Person (val firstName: String, val familyName: String) {
-    val fullName: Any  
-        get(): String { 
-            // error: getter의 반환타입은 property타입 (Any) 와 같아야 한다.
-            return "$firstName $familyName"
-        }
+    // 코틀린 1.1 부터는 property와 getter 정의에서 property타입을 생략하고 타입추론에 의존하면 된다.
+    val fullName
+        get() = "$firstName $familyName"
 }

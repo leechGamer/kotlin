5b545a983eb3e0c2512bb3fa717b42f74dcc026b
g <ruddmsdl000@naver.com>
2023-05-29 21:44:11 +0900

    4.3.3 getter의 반환타입은 property타입과 일치해야한다

--- 4/4.3/Main.kt
+++ 4/4.3/Main.kt
@@ -1,4 +1,7 @@
 class Person (val firstName: String, val familyName: String) {
-    val fullName: String
-        get() = "$firstName $familyName"
+    val fullName: Any  
+        get(): String { 
+            // error: getter의 반환타입은 property타입 (Any) 와 같아야 한다.
+            return "$firstName $familyName"
+        }
 }

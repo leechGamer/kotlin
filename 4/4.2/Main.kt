f84d83932658555b0f24ecc6cf2e70d37833e4d2
g <ruddmsdl000@naver.com>
2023-05-29 18:23:30 +0900

    4.2.1 nullable 타입은 notnull 타입에 들어 있는 어떤 property나 메서드를 제공하지 않는다.

--- 4/4.2/Main.kt
+++ 4/4.2/Main.kt
@@ -1,14 +1,8 @@
-fun isLetterString(s: String): Boolean {
-    if (s.isEmpty()) return  false
-    for (ch in s) {
-        if (! ch.isLetter()) {
-            return false
-        }
+fun isLetterString(s: String?): Boolean {
+    if (s.isEmpty()) return false // Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type String?
+    
+    for (ch in s) { // Not nullable value required to call an 'iterator()' method on for-loop range
+        if (!ch.isLetter()) return false
     }
     return true
-}
-
-fun main() {
-  println(isLetterString("abc"))
-  println(isLetterString(null)) // Null can not be a value of a non-null type String
 }

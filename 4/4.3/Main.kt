440d8bf333aa3c93f8aae77a73cc6626ef7b1134
g <ruddmsdl000@naver.com>
2023-05-29 21:41:58 +0900

    4.3.3 custom getter 예제 (식이 본문인 형태)

--- 4/4.3/Main.kt
+++ 4/4.3/Main.kt
@@ -1,7 +1,4 @@
 class Person (val firstName: String, val familyName: String) {
     val fullName: String
-        get(): String {
-            return "$firstName $familyName"
-        }
-
+        get() = "$firstName $familyName"
 }

98e3e57c8ab52da842243f23b4846c2d93867b98
g <ruddmsdl000@naver.com>
2023-05-29 20:21:46 +0900

    4.2.5 엘비스 연산자 예제3

--- 4/4.2/Main.kt
+++ 4/4.2/Main.kt
@@ -1,1 +1,13 @@
-val n = readLine()?.toInt() ?: 0 
+class Name(val firstName: String, val familyName: String?)
+
+class Person(val name: Name?) {
+    fun describe(): String {
+        val currentName = name ?: return "Unknown"
+        return "${currentName.firstName} ${currentName.familyName}"
+    }
+}
+
+fun main() {
+    println(Person(Name("jh", "kim")).describe())
+    println(Person(null).describe())
+}

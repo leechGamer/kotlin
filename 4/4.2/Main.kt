2a86d68222bd5626e8c64792d49af1c0aa5498e5
g <ruddmsdl000@naver.com>
2023-05-29 19:20:32 +0900

    4.2.2 스마트캐스트 예제

--- 4/4.2/Main.kt
+++ 4/4.2/Main.kt
@@ -1,12 +1,18 @@
-fun isLetterString(s: String?): Boolean {
-    if (s == null) return false
-    if (s.isEmpty()) return false
-    for (ch in s) {
-        if (!ch.isLetter()) return false
-    }
-    return true
+fun describeNumber(n: Int?) = when(n) {
+    null -> "null"
+    in 0..10 -> "small"
+    in 11 .. 100 -> "large"
+    else -> "out of range"
 }
 
+fun isSingleChar(s: String?) = s != null && s.length == 1
+
+
 fun main() {
-    isLetterString(null)
+    var s = readLine()
+    if (s != null) {
+        s = readLine()
+        // s값이 바껴서 스마트 캐스트 할 수 없다.
+        println(s.length) // Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type String
+    }
 }

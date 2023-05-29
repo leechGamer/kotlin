2cd3a0a54888811061b7fd9de1c64f0a9619d52a
g <ruddmsdl000@naver.com>
2023-05-29 18:25:24 +0900

    4.2.1 nullable 타입은 코틀린의 확장 메커니즘을 활용해 자체적인 메서드와 property를 제공한다.

--- 4/4.2/Main.kt
+++ 4/4.2/Main.kt
@@ -1,8 +1,6 @@
-fun isLetterString(s: String?): Boolean {
-    if (s.isEmpty()) return false // Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type String?
-    
-    for (ch in s) { // Not nullable value required to call an 'iterator()' method on for-loop range
-        if (!ch.isLetter()) return false
-    }
-    return true
+fun exclaim(s: String?) {
+    println(s + "!")
+}
+fun main() {
+    exclaim(null) // null!
 }

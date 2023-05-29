e1cdd034901b4238f67fdb9145a2a675891459ab
g <ruddmsdl000@naver.com>
2023-05-29 19:45:07 +0900

    4.2.3 not null assertion 사용이 타당할때의 예제

--- 4/4.2/Main.kt
+++ 4/4.2/Main.kt
@@ -1,18 +1,13 @@
-fun describeNumber(n: Int?) = when(n) {
-    null -> "null"
-    in 0..10 -> "small"
-    in 11 .. 100 -> "large"
-    else -> "out of range"
-}
-
-fun isSingleChar(s: String?) = s != null && s.length == 1
-
-
 fun main() {
-    var s = readLine()
-    if (s != null) {
-        s = readLine()
-        // s값이 바껴서 스마트 캐스트 할 수 없다.
-        println(s.length) // Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type String
+    var name: String? = null
+    fun init() {
+        name = "John"
     }
+
+    fun sayHello() {
+        println(name!!.uppercase())
+    }
+
+    init()
+    sayHello() // JOHN
 }

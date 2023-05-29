6e9d9691379939202119783ad01febb958db47e2
g <ruddmsdl000@naver.com>
2023-05-29 19:10:53 +0900

    4.2.2 null 가능성 예제

--- 4/4.2/Main.kt
+++ 4/4.2/Main.kt
@@ -1,6 +1,12 @@
-fun exclaim(s: String?) {
-    println(s + "!")
+fun isLetterString(s: String?): Boolean {
+    if (s == null) return false
+    if (s.isEmpty()) return false
+    for (ch in s) {
+        if (!ch.isLetter()) return false
+    }
+    return true
 }
+
 fun main() {
-    exclaim(null) // null!
+    isLetterString(null)
 }

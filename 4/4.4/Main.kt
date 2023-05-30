b51270de2a723f32aad312b17ca17fb6817df245
g <ruddmsdl000@naver.com>
2023-05-30 21:15:33 +0900

    4.4.3 object를 함수 안에 정의할 수는 없다.

--- 4/4.4/Main.kt
+++ 4/4.4/Main.kt
@@ -1,14 +1,7 @@
-
-class Application private constructor(val name: String){ // 생성자를 비공개로 지정
-    companion object {
-        fun create(args: Array<String>): Application?{
-            val name = args.firstOrNull()?: return null
-            return Application(name)
-        }
+fun printMiddle(xRange: IntRange, yRange: IntRange) {
+    object MidPoint { // Named object 'MidPoint' is a singleton and cannot be local. Try to use anonymous object instead
+        val x = (xRange.first + xRange.last) / 2
+        val y = (yRange.first + yRange.last) / 2
     }
-}
-
-fun main(args: Array<String>) {
-    val app = Application.create(args) ?: return
-    println("Application started:" ${app.name})
+    println("${MidPoint.x}, ${MidPoint.y}")
 }

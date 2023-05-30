5c87857692fc417b63ac31e7df8f573d6ee13f74
g <ruddmsdl000@naver.com>
2023-05-30 21:26:57 +0900

    4.4.4 익명 객체 타입은 지역 선언이나 비공개 선언에만 전달될 수 있다

--- 4/4.4/Main.kt
+++ 4/4.4/Main.kt
@@ -1,8 +1,10 @@
-// 객체식의 타입이 익명 객체 타입이며 지역변수나 프로퍼티의 타입도 마찬가지 일 수 있음을 보여준다
+// 익명 객체 타입은 지역 선언이나 비공개 선언에만 전달될 수 있다
+
+fun midPoint(xRange: IntRange, yRange: IntRange) = object {
+    val x = (xRange.first + xRange.last)/2
+    val y = (xRange.first + xRange.last)/2
+}
 fun main() {
-    val o = object { // 익명 객체 타입이 추론된다.
-        val x = readLine()!!.toInt()
-        val y = readLine()!!.toInt()
-    }
-    println(o.x + o.y)
+    val mid = midPoint(1..5, 2..6)
+    println("${mid.x}, ${mid.y}") // Unresolved reference: x, y
 }

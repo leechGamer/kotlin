a7fb512601229ea686866691471d47e75895768b
g <ruddmsdl000@naver.com>
2023-06-04 21:04:14 +0900

    5.1.1 정수 배열의 원소의 합계를 계산하는 함수

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,1 +1,7 @@
-
+fun sum(numbers: IntArray): Int {
+    var result = numbers.firstOrNull() ?: throw IllegalArgumentException("empty")
+    
+    for (i in 1..numbers.lastIndex) result += numbers[i]
+    
+    return result
+}

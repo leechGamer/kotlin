c945e82be3cdb7c1d88ec7b32de375959a417796
g <ruddmsdl000@naver.com>
2023-06-05 00:27:09 +0900

    5.1.4  오버로딩된 함수 중 어떤 함수를 참조할지 명확히하려면 타입을 지정

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,13 +1,5 @@
-fun check(s: String, condition: (Char)-> Boolean) : Boolean {
-    for (c in s) {
-        if (!condition(c)) return false
-    }
-    return true
-}
-fun isCapitalLetter(c: Char) = c.isUpperCase() && c.isLetter()
+fun max(a: Int, b: Int) = if (a > b) a else b
+fun max(a: Double, b: Double) = if (a > b) a else b
 
-fun main() {
-    println(check("hello") {c -> isCapitalLetter(c)})
-    println(check("hello") {isCapitalLetter(it)})
-    println(check("hello", ::isCapitalLetter(it))
-}
+val f: (Int, Int) -> Int = ::max
+val g = ::max // Overload resolution ambiguity. All these functions match.

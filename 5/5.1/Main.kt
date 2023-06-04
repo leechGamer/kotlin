0f49106734c0aa06e153cc36f5ecdd8181ed4ad1
g <ruddmsdl000@naver.com>
2023-06-04 22:18:00 +0900

    5.1.3 람다파라미터를 밑줄 기호로 지정할 수 있다

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,21 +1,12 @@
-// 람다에 인자가 없으면 화살표 기호를 생략할 수 있다.
-fun measureTime(action: () -> Unit): Long {
-    val start = System.nanoTime()
-    action()
-    return System.nanoTime() - start
-}
-
-val time = measureTime { 1 +2 }
-
-// 코틀린은 인자가 하나밖에 없는 람다를 특별히 단순화해 사용할 수 있는 문법을 제공한다.
-fun check(s: String, condition: (Char) -> Boolean) : Boolean {
-    for (c in s) {
-        if (!condition(c)) return false
+// 람다 파라미터를 밑줄 기호로 지정할 수 있다.
+fun check(s: String, condition: (Int, Char) -> Boolean) : Boolean {
+    for (i in s.indices) {
+        if (!condition(i, s[i])) return false
     }
     return true
 }
 
 fun main() {
-    println(check("hello") { c -> c.isLetter() })
-    println(check("hello") { it.isLowerCase() })
+    println(check("hello") { _, c -> c.isLetter() })
+    println(check("hello") { i, c -> i == 0 || c.isLowerCase() })
 }

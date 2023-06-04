e0c7a0f5a18028a9e993837f3a32d216782eeb72
g <ruddmsdl000@naver.com>
2023-06-04 21:34:59 +0900

    5.1.1 일반화를 통한 다양한 집계함수

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,7 +1,15 @@
-fun sum(numbers: IntArray): Int {
+fun sum(numbers: IntArray) = aggregate(numbers) { result, op -> result + op }
+
+fun aggregate(numbers: IntArray, op: (Int, Int) -> Int): Int {
     var result = numbers.firstOrNull() ?: throw IllegalArgumentException("empty")
-    
-    for (i in 1..numbers.lastIndex) result += numbers[i]
-    
+
+    for (i in 1..numbers.lastIndex) result = op(result, numbers[i])
     return result
+}
+
+fun max(numbers: IntArray) = aggregate(numbers) { result, op -> if (op > result) op else result }
+
+fun main() {
+    println(sum(intArrayOf(1,2,3))) // 6
+    println(max(intArrayOf(1,2,3))) // 3
 }

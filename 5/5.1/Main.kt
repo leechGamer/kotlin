5df1279566e348c082885fc320388cb29e86fa68
g <ruddmsdl000@naver.com>
2023-06-04 22:13:39 +0900

    5.1.2 인자가 없거나 하나밖에 없을때

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,15 +1,21 @@
-fun sum(numbers: IntArray) = aggregate(numbers) { result, op -> result + op }
-
-fun aggregate(numbers: IntArray, op: (Int, Int) -> Int): Int {
-    var result = numbers.firstOrNull() ?: throw IllegalArgumentException("empty")
-
-    for (i in 1..numbers.lastIndex) result = op(result, numbers[i])
-    return result
+// 람다에 인자가 없으면 화살표 기호를 생략할 수 있다.
+fun measureTime(action: () -> Unit): Long {
+    val start = System.nanoTime()
+    action()
+    return System.nanoTime() - start
 }
 
-fun max(numbers: IntArray) = aggregate(numbers) { result, op -> if (op > result) op else result }
+val time = measureTime { 1 +2 }
+
+// 코틀린은 인자가 하나밖에 없는 람다를 특별히 단순화해 사용할 수 있는 문법을 제공한다.
+fun check(s: String, condition: (Char) -> Boolean) : Boolean {
+    for (c in s) {
+        if (!condition(c)) return false
+    }
+    return true
+}
 
 fun main() {
-    println(sum(intArrayOf(1,2,3))) // 6
-    println(max(intArrayOf(1,2,3))) // 3
+    println(check("hello") { c -> c.isLetter() })
+    println(check("hello") { it.isLowerCase() })
 }

e0e2f2dacab05001d31249cd4951eedd8babf257
g <ruddmsdl000@naver.com>
2023-06-05 21:25:20 +0900

    5.1.5 noline을 붙이면 특정 람다를 인라인하지 말라고 할 수있다.

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,5 +1,5 @@
-// 인라인 함수가 널이 될 수 있는 함수 타입의 인자를 받을 수도 없다
-inline fun forEach(a: IntArray, action: ((Int) -> Unit)?) { // Inline-parameter 'action: ((Int) -> Unit)?' of 'public inline fun forEach(a: IntArray, action: ((Int) -> Unit)?): Unit defined in root package in file Calc.kt' must not be nullable. Add 'noinline' modifier to the parameter declaration or make its type not nullable
+// noline을 붙이면 특정 람다를 인라인하지 말라고 할 수있다.
+inline fun forEach(a: IntArray, noinline action: ((Int) -> Unit)?) {
     if (action == null) return
     for (n in a) action(n)
 }

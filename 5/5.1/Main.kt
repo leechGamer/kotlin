892b641e08ef3afcbb2aeacac4fda28625fe360b
g <ruddmsdl000@naver.com>
2023-06-05 21:48:09 +0900

    5.1.5 property가 public인 경우 getter / setter안에서 비공개 선언을 참조하면 인라인이 불가능하다

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,8 +1,8 @@
+// property가 public인 경우 getter / setter안에서 비공개 선언을 참조하면 인라인이 불가능하다
 class Person (
-    val firstName: String,
-    val familyName: String,
+    private val firstName: String,
+    private val familyName: String,
 ){
-    inline var fullName
-        get() = "$firstName $familyName"
-        set(value) {}
+    inline var age = 0 // Error: initializer is not allowed here because this property has no backing field
+    inline val fullName get() = "$firstName $familyName" // Error: Public-API inline function cannot access non-public-API 'private final val familyName: String defined in Person'
 }

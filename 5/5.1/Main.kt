a5fac2896ee8d64043c94772fbb0bd0dbd71c508
g <ruddmsdl000@naver.com>
2023-06-05 21:23:18 +0900

    5.1.5 인라인 함수가 널이 될 수 있는 함수 타입의 인자를 받을 수도 없다

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,10 +1,5 @@
-// 인라인이 될 수 있는 람다를 사용해 할 수 있는 일은
-// 1. 람다를 호출하거나
-// 2. 다른 인라인 함수에 인라인이 되도록 넘기는 두가지 경우뿐이다.
-
-var lastAction: () -> Unit = {}
-
-inline fun runAndMemorize(action: () -> Unit) {
-    action()
-    lastAction = action // Illegal usage of inline-parameter 'action' in 'public inline fun runAndMemorize(action: () -> Unit): Unit defined in root package in file Calc.kt'. Add 'noinline' modifier to the parameter declaration
+// 인라인 함수가 널이 될 수 있는 함수 타입의 인자를 받을 수도 없다
+inline fun forEach(a: IntArray, action: ((Int) -> Unit)?) {
+    if (action == null) return
+    for (n in a) action(n)
 }

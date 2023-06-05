aa06df7382cc683aeefe98727f1088a991bc7d80
g <ruddmsdl000@naver.com>
2023-06-05 21:38:37 +0900

    5.1.5 코틀린 1.1에서 부터 property 접근자를 인라인하도록 허용 한다.

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,9 +1,8 @@
-// 비공개 코드가 외부로 노출되는 일을 방지하기 위해 코틀린은 인라인 함수에 비공개 멤버를 전달하는 것을 금지한다.
 class Person (
-private val firstName: String,
-private val familyName: String,
+    val firstName: String,
+    val familyName: String,
 ){
-    inline fun secondMessage(message: () -> String) {
-        println("$firstName $familyName: ${message()}") // ublic-API inline function cannot access non-public-API 'private final val firstName: String defined in Person
-    }
+    var fullName
+        inline get() = "$firstName $familyName"
+        set(value) {}
 }

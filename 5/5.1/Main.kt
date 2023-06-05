f714409fc22750cae5ce4a743542a71f12f38b9a
g <ruddmsdl000@naver.com>
2023-06-05 21:20:07 +0900

    5.1.5 인라인이 될 수 있는 람다를 사용한 케이스

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,27 +1,10 @@
-// inline 기법 : 함수 값을 사용하는 고차 함수를 호출하는 부분을 해당 함수의 본문으로 대체
+// 인라인이 될 수 있는 람다를 사용해 할 수 있는 일은
+// 1. 람다를 호출하거나
+// 2. 다른 인라인 함수에 인라인이 되도록 넘기는 두가지 경우뿐이다.
 
-inline fun indexOf(numbers: IntArray, condition: (Int) -> Boolean): Int {
-    for (i in numbers.indices) {
-        if (condition(numbers[i])) return 1
-    }
-    return -1
-}
+var lastAction: () -> Unit = {}
 
-fun main() {
-    println(indexOf(intArrayOf(4, 3, 2, 1)) { it < 3 })
-}
-
-// 위 코드의 ----------- main()은 아래와 같이 해석 되었다.
-
-fun main() {
-    val numbers = intArrayOf(4, 3, 2, 1)
-    var index = -1
-
-    for (i in numbers.indices) {
-        if (numbers[i] < 3) {
-            index = i
-            break
-        }
-    }
-    println(index)
+inline fun runAndMemorize(action: () -> Unit) {
+    action()
+    lastAction = action // Illegal usage of inline-parameter 'action' in 'public inline fun runAndMemorize(action: () -> Unit): Unit defined in root package in file Calc.kt'. Add 'noinline' modifier to the parameter declaration
 }

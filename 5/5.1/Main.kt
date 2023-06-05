81e375380fb7445dcf360ef470835088aaf4c530
g <ruddmsdl000@naver.com>
2023-06-05 20:59:18 +0900

    5.1.5 인라인 기법 예제

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -1,5 +1,12 @@
-fun max(a: Int, b: Int) = if (a > b) a else b
-fun max(a: Double, b: Double) = if (a > b) a else b
+// inline 기법 : 함수 값을 사용하는 고차 함수를 호출하는 부분을 해당 함수의 본문으로 대체
 
-val f: (Int, Int) -> Int = ::max
-val g = ::max // Overload resolution ambiguity. All these functions match.
+inline fun indexOf(numbers: IntArray, condition: (Int) -> Boolean): Int {
+    for (i in numbers.indices) {
+        if (condition(numbers[i])) return 1
+    }
+    return -1
+}
+
+fun main() {
+    println(indexOf(intArrayOf(4, 3, 2, 1)) { it < 3 })
+}

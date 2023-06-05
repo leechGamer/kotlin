2ddb099ffef490ce94798d0bf5a865c236d44f96
g <ruddmsdl000@naver.com>
2023-06-05 21:00:51 +0900

    5.1.5 인라인 함수 해석

--- 5/5.1/Main.kt
+++ 5/5.1/Main.kt
@@ -10,3 +10,18 @@ inline fun indexOf(numbers: IntArray, condition: (Int) -> Boolean): Int {
 fun main() {
     println(indexOf(intArrayOf(4, 3, 2, 1)) { it < 3 })
 }
+
+// 위 코드의 ----------- main()은 아래와 같이 해석 되었다.
+
+fun main() {
+    val numbers = intArrayOf(4, 3, 2, 1)
+    var index = -1
+
+    for (i in numbers.indices) {
+        if (numbers[i] < 3) {
+            index = i
+            break
+        }
+    }
+    println(index)
+}

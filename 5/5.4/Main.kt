a5600052fb5def3d7f4bf417e0a42bef6b8b5b3d
g <ruddmsdl000@naver.com>
2023-06-06 17:04:55 +0900

    5.4 IntRange클래스의 companion object에 대한 확장 함수를 정의 예제

--- 5/5.4/Main.kt
+++ 5/5.4/Main.kt
@@ -1,1 +1,7 @@
+// IntRange클래스의 companion object에 대한 확장 함수를 정의한다.
+fun IntRange.Companion.singletonRange(n: Int) = n..n
 
+fun main() {
+    println(IntRange.singletonRange(5))
+    println(IntRange.Companion.singletonRange(3))
+}

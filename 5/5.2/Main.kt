a67517169413fc4fc61bab7776ab599f7dd9e7e7
g <ruddmsdl000@naver.com>
2023-06-06 15:57:24 +0900

    5.2.1 지역확장 함수를 정의

--- 5/5.2/Main.kt
+++ 5/5.2/Main.kt
@@ -1,11 +1,20 @@
-// 클래스 멤버와 확장의 시그니처가 같다면?
-// 호출 지점에서 어떤 함수를 호출할지 모호할 때 컴파일러는 항상 멤버 함수를 우선적으로 선택한다. 
-class Person(val firstName: String, val familyName: String) {
-     fun fullName() = "$firstName $familyName"
+// 지역확장 함수를 정의할 수 있다. 
+
+interface Truncated {
+    val truncated: String,
+    val original: String,
 }
 
-fun Person.fullName() = "$firstName $familyName"
+private fun String.truncator(max: Int) = object: Truncated{
+    override val truncated: String
+        get() = if(length <= max) this@truncator else substring(0, max)
+    override val original: String
+        get() = this@truncator
+
+}
 
 fun main() {
-    println(Person("John", "Doe").fullName())
+    val truncator = "hello".truncator(3)
+    println(truncator.original)
+    println(truncator.truncated)
 }

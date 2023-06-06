b0b57d2444ab5db464a672dd0e42308e0923a848
g <ruddmsdl000@naver.com>
2023-06-06 19:16:16 +0900

    5.5.1 with() 함수 예제

--- 5/5.5/5.5.1/Main.kt
+++ 5/5.5/5.5.1/Main.kt
@@ -1,12 +1,11 @@
-// run
-// 객체 상태를 설정한 다음 객체를 대상으로 어떤 결과를 만들어 내느 람다를 호출
-
-class Address {
-    var zipCode: Int =0
-    var city: String = ""
-    var street: String = ""
-    var house: String = ""
-
+// with
+// run 과 비슷하지만 확장함수 타입이 아니므로 수신객체를 첫번째 인자로 넘겨야한다.
+class Address (
+    var zipCode: Int =0,
+    var city: String = "",
+    var street: String = "",
+    var house: String = "",
+) {
     fun post(message: String): Boolean {
         "Message for {$zipCode, $city, $street, $house} : $message"
 
@@ -15,16 +14,14 @@ class Address {
 }
 
 fun main() {
-    val isReceived = Address().run {
-    // addr 인스턴스를 this로 사용할 수 있음
-        zipCode = 123
-        city = "seoul"
-        street = "baker st"
+    val message = with(Address(
+        zipCode = 123,
+        city = "seoul",
+        street = "baker st",
         house = "221bn"
-        post("hello !!")
+    )) {
+        "Address : $city"
     }
 
-    if (!isReceived) {
-        println("Message is nto delivered")
-    }
+    println(message)
 }

32eeb260e8aacd9209ea335f0b735756042e63a0
g <ruddmsdl000@naver.com>
2023-06-06 15:43:53 +0900

    5.2.1 확장함수: 클래스 멤버와 확장의 시그니처가 같을때

--- 5/5.2/Main.kt
+++ 5/5.2/Main.kt
@@ -1,10 +1,11 @@
-// binding 된 호출 가능 참조 위치에 사용할 수도 있다
-class Person(val name: String, val age: Int)
+// 클래스 멤버와 확장의 시그니처가 같다면?
+// 호출 지점에서 어떤 함수를 호출할지 모호할 때 컴파일러는 항상 멤버 함수를 우선적으로 선택한다. 
+class Person(val firstName: String, val familyName: String) {
+     fun fullName() = "$firstName $familyName"
+}
 
-fun Person.hasName(name: String) = name.equals(this.name, ignoreCase = true)
+fun Person.fullName() = "$firstName $familyName"
 
 fun main() {
-    val f = Person("John", 25)::hasName //
-    println(f("John"))
-    println(f("Jake"))
+    println(Person("John", "Doe").fullName())
 }

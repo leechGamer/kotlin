209446cdbf174c54d9d23041193118f379691be1
g <ruddmsdl000@naver.com>
2023-06-06 18:40:52 +0900

    5.5 일반 함수 타입의 파라미터를 받는 함수에 호출가능 참조를 전달할 수 있다.

--- 5/5.5/Main.kt
+++ 5/5.5/Main.kt
@@ -1,15 +1,20 @@
-// 수신 객체가 없는 호출 가능 참조를
-// 수신 객체가 필요한 함수 타입의 값 대신 사용 가능
-// Int.(Int) -> Int 타입의 파라미터를 받는 고차 함수에 인자가 두 개인 :: max를 전달해도 됨
-fun aggregate(numbers: IntArray, op: Int.(Int) -> Int): Int {
-    var result = numbers.firstOrNull()
-        ?: throw IllegalArgumentException("empty Array")
 
-    for(i in  1.. numbers.lastIndex) result = result.op(numbers[i])
+// 수신 객체가 아닌 일반 함수 타입의 파라미터를 받는 함수에
+// 수신 객체가 지정된 호출가능 참조를 전달할 수도 있다
+fun aggregate(numbers: IntArray, op: (Int, Int) -> Int) : Int {
+    var result = numbers.firstOrNull()
+        ?: throw IllegalArgumentException()
+
+
+    for (i in 1.. numbers.lastIndex) result = op(result, numbers[i])
 
     return result
 }
-fun max(a: Int, b: Int) = if(a > b) a else b
+
+fun Int.max(other: Int) = if(this > other) this else other
+
 fun main() {
-    println(aggregate(intArrayOf(1,2,3,4), ::max))
+    println(aggregate(intArrayOf(1,2,3,4), Int::max)) // 멤보에 대한 호출 가능참조
+    println(aggregate(intArrayOf(1,2,3,4), Int::plus)) // 확장 함수에 대한 호출 가능 참조
 }
+

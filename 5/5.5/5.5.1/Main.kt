76c2a510a3fdc13937f0db4c81c059998cbc25b4
g <ruddmsdl000@naver.com>
2023-06-07 23:35:14 +0900

    5.5.1 apply 함수

--- 5/5.5/5.5.1/Main.kt
+++ 5/5.5/5.5.1/Main.kt
@@ -1,15 +1,17 @@
-// let 함수: 처리해야 하는 값을 무조건 널이 될 수 없는 값으로만 전달해야 한다면
+// apply 함수 : 반환값을 만들어내지 않고 객체 상태를 설정
+class Address {
+    var city: String = ""
+    var street: String = ""
+    var house: String = ""
 
-import java.lang.NumberFormatException
-
-fun readInt() = try {
-    readLine()?.toInt()
-} catch (e: NumberFormatException) {
-    null
+    fun post(message: String) {}
 }
 
-fun main(args: Array<String>) {
-    val index = readInt()
-    val arg = index?.let { args.getOrNull(it) } // let을 활용해 간단히 notnull로 표현
-    if (arg != null) println(arg)
+fun main() {
+    val  message = readLine()?: return
+    Address().apply {
+        city = "London"
+        street = "Baker Street"
+        house = "221b"
+    }.post(message)
 }

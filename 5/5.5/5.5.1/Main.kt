f44bae78044eb276c27986b0f700b3f32e123268
g <ruddmsdl000@naver.com>
2023-06-07 22:27:00 +0900

    5.5.1 문맥이 없는 run

--- 5/5.5/5.5.1/Main.kt
+++ 5/5.5/5.5.1/Main.kt
@@ -1,27 +1,10 @@
-// 문맥 없는 run
-// run 을 오버로딩
-// 문맥 식이 없고 람다 값을 반환하기만 함
-class Address (
-    var zipCode: Int =0,
-    var city: String = "",
-    var street: String = "",
-    var house: String = "",
-) {
-    fun post(message: String): Boolean {
-        "Message for {$zipCode, $city, $street, $house} : $message"
+// 문맥이 없는 run
 
-        return readln() == "OK"
-    }
-
-    fun asText() = "$city $street $house"
+class Address(val city: String, val street: String, val house: String) {
+    fun asText() = "$city, $street, $house"
 }
 
 fun main() {
-    val addr = Address(
-        zipCode = 123,
-        city = "seoul",
-        street = "baker st",
-        house = "221bn"
-    )
-    println(addr.asText())
+    val address = Address("London", "Baker Street", "221b")
+    println(address.asText())
 }

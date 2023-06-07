a774924c86416cfaf1364846a50d9c77c6e6d8fe
g <ruddmsdl000@naver.com>
2023-06-07 22:59:27 +0900

    5.5.1 let 함수

--- 5/5.5/5.5.1/Main.kt
+++ 5/5.5/5.5.1/Main.kt
@@ -1,10 +1,15 @@
-// 문맥이 없는 run
-
+// let 함수
 class Address(val city: String, val street: String, val house: String) {
-    fun asText() = "$city, $street, $house"
+    fun post(message: String) {}
 }
-
 fun main() {
-    val address = Address("London", "Baker Street", "221b")
-    println(address.asText())
+    Address("London", "Baker Street", "221b").let {
+        println("To city: ${it.city}")
+        it.post("Hello")
+    }
+    
+     Address("London", "Baker Street", "221b").let { addr -> 
+        println("To city: ${it.city}")
+        it.post("Hello")
+    }
 }

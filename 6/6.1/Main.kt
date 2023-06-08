c5e9ba7fd4fda6c0a6359a6084785861b4f59afe
g <ruddmsdl000@naver.com>
2023-06-08 22:19:07 +0900

    6.1.2. enum 본문에 정의된 멤버를 해당 본문이 아닌 부분에 접근할 수 없다

--- 6/6.1/Main.kt
+++ 6/6.1/Main.kt
@@ -1,11 +1,7 @@
-// 6.1.2 enum class에 생성자가 있으면 각 enum 상수의 정의 뒤에도 적절한 생성자 호출을 추가해야 한다. 
-enum class RainbowColor(val isCold: Boolean) {
-    RED(false), ORANGE(false), YELLOW(false), GREEN(true), BLUE(true), INDIGO(true), VIOLET(true);
-    
-    val isWarm get() = !isCold
+// 6.1.2 enum 의 본문에 정의된 멤버를 해당 본문이 아닌 부분에서 접근할 수 없다
+enum class WeekDay {
+    MONDAY { fun startWork() = println("work week started") },
+    TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 }
 
-fun main() {
-    println(RainbowColor.RED.isCold)
-    println(RainbowColor.GREEN.isWarm)
-}
+fun main() = WeekDay.MONDAY.startWork() // Error:: Unresolved reference: startWork

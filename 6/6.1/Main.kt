1d4a3ca1912143c0cd21d3dbbcffd661b1ac4ca2
g <ruddmsdl000@naver.com>
2023-06-08 21:55:29 +0900

    6.1.2 커스텀 멤버가 있는 enum 정의하기

--- 6/6.1/Main.kt
+++ 6/6.1/Main.kt
@@ -1,10 +1,12 @@
+// 6.1.2 커스텀 멤버가 있는 enum 정의하기
 enum class WeekDay {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+
+    val lowerCaseName get() = name.lowercase()
+    fun isWorkday() = this == SATURDAY || this == SUNDAY
 }
 
-fun WeekDay.isWorkDay() = this == WeekDay.SATURDAY || this == WeekDay.SUNDAY
-
 fun main() {
-    println(WeekDay.MONDAY.isWorkDay())
-    println(WeekDay.SATURDAY.isWorkDay())
+    println(WeekDay.MONDAY.isWorkday())
+    println(WeekDay.WEDNESDAY.lowerCaseName)
 }

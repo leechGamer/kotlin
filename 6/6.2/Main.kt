ddb7a896a935d84c4943c9c317d8e4ca1f4a54bd
g <ruddmsdl000@naver.com>
2023-06-11 19:10:38 +0900

    6.2 주생성자의 파라미터에서 선어한 프로퍼티만 equals() / hashCod()/ toString() 메서드 구현에 쓰인다.

--- 6/6.2/Main.kt
+++ 6/6.2/Main.kt
@@ -1,8 +1,12 @@
-// data class는 toString() 구현도 생성해준다.
-data class Person(val firstName: String, val familyName: String, val age: Int)
+// 주생성자의 파라미터에서 선어한 프로퍼티만 equals() / hashCod()/ toString() 메서드 구현에 쓰인다.
+// 다른 프로퍼티들은 이런 함수들의 생성에 영향을 미치지 못한다.
+data class Person(val firstName: String, val familyName: String) {
+    var age = 0
+}
 
 fun main() {
-    val person = Person("john", "doe", 25)
-    println(person) // Person(firstName=john, familyName=doe, age=25)
+    val person = Person("john", "doe").apply { age = 25 }
+    val person2 = Person("john", "doe").apply { age = 26 }
+    println(person == person2) // Person(firstName=john, familyName=doe, age=25)
 
 }

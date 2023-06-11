a2925336ddd90fa097fc3e433bf299a87d456697
g <ruddmsdl000@naver.com>
2023-06-11 18:03:10 +0900

    6.2.1 class -> data class 변경 시 컴파일러가 주생성자에 정의된 프로퍼티의 값을 서로 비교하는 등등성 비교연산을 자동으로 생성해준다.

--- 6/6.2/Main.kt
+++ 6/6.2/Main.kt
@@ -1,11 +1,12 @@
-// 자바와 마찬가지로 타입의 값은 기본적으로 참조가 가리키는 객체의 identity가 같으면 같다. (즉, 메모리에서의 주소값이 같으면 같다)
-class Person(val firstName: String, val familyName: String, val age: Int)
+// class -> data class 컴파일러가 주생성자에 정의된 
+// 프로퍼티의 값을 서로 비교하는 동등성 비교 연산을 자동으로 생성해주기 때문에 두 비교가 true가 된다.
+data class Person(val firstName: String, val familyName: String, val age: Int)
 
 fun main() {
     val person1 = Person("jo", "doe", 25)
     val person2 = Person("jo", "doe", 25)
     val person3 = person1
 
-    println(person1 == person2) // false
+    println(person1 == person2) // true
     println(person1 == person3) // true
 }

5da39e8eacb78b37c0fb0a87eea54c56bea6d966
g <ruddmsdl000@naver.com>
2023-06-11 21:15:45 +0900

    6.2.2 구조분해 선언

--- 6/6.2/Main.kt
+++ 6/6.2/Main.kt
@@ -1,16 +1,18 @@
-import kotlin.random.Random
-
 // 구조 분해 선언
 
 data class Person(val first: String, val family: String, val age: Int)
 
-fun newPerson() = Person(readln()!!, readln()!!, Random.nextInt(100))
 
 fun main() {
-    val (first, family, age) = Person("name", "family" , 20)
-    if (age < 18) {
-        println("$first $family is under-age: $age")
-    } else {
-        println("$first $family is upper-age: $age")
-    }
+    val (name) = Person("john", "doe", 25)
+    println(name)
+
+    val (_, family) = Person("john", "doe", 25)
+    println(family)
+
+    var (firstName, familyName) = Person("JOHN", "DOE", 25)
+    firstName = firstName.lowercase()
+    familyName = familyName.lowercase()
+    println("$firstName $familyName") // john doe
 }
+

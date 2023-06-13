fb61cb8135127195f3b5493be508f902cfb8ea66
g <ruddmsdl000@naver.com>
2023-06-13 21:25:35 +0900

    6.3.1 정확히 상은하는 (프로퍼티) 타입의 값으로 사용될 수 있는 경우에 인라인될 수 있다는 규칙이 있다.

--- 6/6.3/Main.kt
+++ 6/6.3/Main.kt
@@ -1,7 +1,11 @@
-// 인라인 클래스 정의하기
-// 1. 인라인 클래스를 정의하려면 value class를 클래스 이름 앞에 붙여야한다.
-// 2. JVM 백엔드를 사용하는 경우에는 @JvmInline을 value class 앞에 반드시 붙여줘야 한다.
+// 인라인 클래스 인스턴스는 다른 어떤 타입의 값으로 캐스팅하지 않고도
+// 정확히 상응하는 타입의 값으로 사용될 수 있는 경우에 인라인될 수 있다는 규칙이 있다.
 @JvmInline
-value class Dollar(val amount: Int)
-@JvmInline
-value class Euro(val amount: Int)
+value class Dollar(val amount: Int) 
+
+fun safeAmount(dollar: Dollar?) = dollar?.amount ?: 0
+fun main() {
+    println(Dollar(15).amount) // 인라인 됨 
+    println(Dollar(15)) // Any? 로 사용되기 때문에 인라인되지 않음 
+    println(safeAmount(Dollar(15)))// Dollar?로 사용되기 때문에 인라인되지 않음
+}

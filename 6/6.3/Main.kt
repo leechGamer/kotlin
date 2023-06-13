ad9f6a45f7eb523abc15c14e997e45f54b76b9e7
g <ruddmsdl000@naver.com>
2023-06-13 21:39:29 +0900

    6.3.2 부호 없는 정수

--- 6/6.3/Main.kt
+++ 6/6.3/Main.kt
@@ -1,11 +1,6 @@
-// 인라인 클래스 인스턴스는 다른 어떤 타입의 값으로 캐스팅하지 않고도
-// 정확히 상응하는 타입의 값으로 사용될 수 있는 경우에 인라인될 수 있다는 규칙이 있다.
-@JvmInline
-value class Dollar(val amount: Int) 
-
-fun safeAmount(dollar: Dollar?) = dollar?.amount ?: 0
-fun main() {
-    println(Dollar(15).amount) // 인라인 됨 
-    println(Dollar(15)) // Any? 로 사용되기 때문에 인라인되지 않음 
-    println(safeAmount(Dollar(15)))// Dollar?로 사용되기 때문에 인라인되지 않음
-}
+// 
+val uByte: UByte = 1u // 명시적으로 UByte
+val uShort: UShort = 100u // 명시적으로 
+val uInt = 1000u // UInt로 추론
+val uLong: ULong = 1000u // 명시적으로 ULong
+val uLong2 = 1000uL // L 접미사가 붙었기에 명시적으로 ULong

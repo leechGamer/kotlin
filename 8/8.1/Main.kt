701c78b4428264f6b0f7dc57380397041d1eb917
g <ruddmsdl000@naver.com>
2023-06-25 21:03:30 +0900

    8.1.2. 상위 클래스가 여러 생성자를 제공하고 하위 클래스에서 상위 클래스의 생성자 중 둘 이상을 지원하고 싶을때

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,10 +1,28 @@
-// 8.1.2 클래스에 주생성자가 있으면 부생성자가 상위 클래스를 위임 호출할 수 없다
-open class Person (val name: String, val age: Int)
+// 상위 클래스가 여러 생성자를 제공하고 하위 클래스에서 상위 클래스의 생성자 중 둘 이상을 지원하고 싶을때
 
-class Student(): Person { // error: This type has a constructor, and thus must be initialized here
-    private val university: String
+open class Person {
+    val name: String
+    val age: Int
+
+    constructor(name: String, age: Int) {
+        this.name = name
+        this.age = age
+    }
+
+    constructor(firstName: String, familyName: String, age: Int) :
+            this("$firstName $familyName", age) {
+    }
+}
+
+class Student: Person {
+    val university: String
     constructor(name: String, age: Int, university: String) :
-            super(name, age) { // error: Primary constructor call expected
+            super(name, age) {
+        this.university = university
+    }
+
+    constructor(firstName: String, familyName: String, age: Int, university: String) :
+            super(firstName, familyName, age) {
         this.university = university
     }
 }

889f644ce4f3de7740abf120002b99b4a03fabf2
g <ruddmsdl000@naver.com>
2023-06-25 18:58:06 +0900

    8.1.1 프로퍼티 오버라이드

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,18 +1,6 @@
-// 8.1.1 오버라이드하는 멤버를 final로 선언하면 더 이상 하위 클래스가 이 멤버를 오버라이드할 수 없다
-open class Vehicle {
-    open fun start() {
-        println("I am moving")
-    }
+// 8.1.1 프로퍼티 오버라이드
+open class Entity {
+    open val name: String get() = ""
 }
 
-open class Car: Vehicle() {
-    final override fun start() {
-        println("I am riding a car")
-    }
-}
-
-class Bus: Car() {
-    override fun start() { // 'start' in 'car' is final and cannot be overridden 
-        println("I am riding a bus")
-    }
-}
+class Person(override val name: String) : Entity()

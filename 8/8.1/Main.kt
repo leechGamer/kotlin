aebefc9b70c91154cc14c4a43481895c89b30d8c
g <ruddmsdl000@naver.com>
2023-06-25 21:14:49 +0900

    8.1.2 가상함수 예제

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,32 +1,29 @@
-// 상위 클래스가 여러 생성자를 제공하고 하위 클래스에서 상위 클래스의 생성자 중 둘 이상을 지원하고 싶을때
-
-open class Person {
-    val name: String
+// 8.1.2 가상함수 예제
+open class Person(
+    val name: String,
     val age: Int
-
-    constructor(name: String, age: Int) {
-        this.name = name
-        this.age = age
+) {
+    open fun show() {
+        println("$name $age")
     }
 
-    constructor(firstName: String, familyName: String, age: Int) :
-            this("$firstName $familyName", age) {
+    init {
+        show()
     }
 }
 
-class Student: Person {
+class Student(
+    name: String,
+    age: Int,
     val university: String
-    constructor(name: String, age: Int, university: String) :
-            super(name, age) {
-        this.university = university
-    }
-
-    constructor(firstName: String, familyName: String, age: Int, university: String) :
-            super(firstName, familyName, age) {
-        this.university = university
+): Person(name, age) {
+    override fun show() {
+        println("$name $age (student at $university)")
     }
 }
 
 fun main() {
-    Student("susan", 25, "MIT")
+    Student("euan", 25, "MIT")
+    // euan 25 (student at null)
+
 }

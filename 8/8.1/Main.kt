fc4751c1b0ea73e218490fd594790df860137a45
g <ruddmsdl000@naver.com>
2023-06-25 18:51:29 +0900

    8.1.1 반환타입을 하위 타입으로 바꾸기

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,27 +1,11 @@
-// 8.1.1 항상 정적으로 알려진 수신객체 타입을 기반으로 초출할 확장을 선택한다. 컴파일러는
+// 8.1.1 반환 타입을 하위 타입으로 바꾸기
 open class Vehicle {
-    open fun start(speed: Int) {
-        println("I am moving at $speed")
-    }
+    open fun start(): String? = null
 }
-
-fun Vehicle.stop() {
-    println("Stopped")
-}
-
 class Car: Vehicle() {
-    override fun start() { // error: 'start' overrides nothing
-        // 시그니처가 달라서 다른 메서드를 오버라이딩하려는 걸로 인식 
-        println("I am riding")
-    }
+    override fun start() = "I am riding a car"
 }
-
-fun Car.stop() {
-    println("Stopped riding")
-}
-
 fun main() {
     val vehicle: Vehicle = Car()
     vehicle.start()
-    vehicle.stop()
 }

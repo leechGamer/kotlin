f407352bd69908edad8d3988ec18c417df3844ff
g <ruddmsdl000@naver.com>
2023-06-25 18:55:38 +0900

    // 8.1.1 오버라이드하는 멤버를 final로 선언하면 더 이상 하위 클래스가 이 멤버를 오버라이드할 수 없다

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,11 +1,18 @@
-// 8.1.1 반환 타입을 하위 타입으로 바꾸기
+// 8.1.1 오버라이드하는 멤버를 final로 선언하면 더 이상 하위 클래스가 이 멤버를 오버라이드할 수 없다
 open class Vehicle {
-    open fun start(): String? = null
+    open fun start() {
+        println("I am moving")
+    }
 }
-class Car: Vehicle() {
-    override fun start() = "I am riding a car"
+
+open class Car: Vehicle() {
+    final override fun start() {
+        println("I am riding a car")
+    }
 }
-fun main() {
-    val vehicle: Vehicle = Car()
-    vehicle.start()
+
+class Bus: Car() {
+    override fun start() { // 'start' in 'car' is final and cannot be overridden 
+        println("I am riding a bus")
+    }
 }

5d69f4cd6dbedf707c16ccf212eb7fd059ba2643
g <ruddmsdl000@naver.com>
2023-06-25 18:48:23 +0900

    8.1.1

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,7 +1,7 @@
 // 8.1.1 항상 정적으로 알려진 수신객체 타입을 기반으로 초출할 확장을 선택한다. 컴파일러는
 open class Vehicle {
-    open fun start() {
-        println("I am moving")
+    open fun start(speed: Int) {
+        println("I am moving at $speed")
     }
 }
 
@@ -10,7 +10,8 @@ fun Vehicle.stop() {
 }
 
 class Car: Vehicle() {
-    override fun start() {
+    override fun start() { // error: 'start' overrides nothing
+        // 시그니처가 달라서 다른 메서드를 오버라이딩하려는 걸로 인식 
         println("I am riding")
     }
 }

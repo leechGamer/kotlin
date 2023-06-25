e3e2dd2fd7d832e52777f0985b52b1ba6d41182e
g <ruddmsdl000@naver.com>
2023-06-25 19:02:44 +0900

    8.1.1 protected

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,6 +1,20 @@
-// 8.1.1 프로퍼티 오버라이드
-open class Entity {
-    open val name: String get() = ""
+// 8.1.1 protected
+open class Vehicle {
+    protected open fun onStart() {} // 하위클래스만 접근 가능
+    fun start() {
+        println("Starting up..")
+        onStart()
+    }
 }
 
-class Person(override val name: String) : Entity()
+class Car: Vehicle() {
+    override fun onStart() {
+        println("Its a car")
+    }
+}
+
+fun main() {
+    val car = Car()
+    car.start()
+    car.onStart() // Cannot access 'onStart': it is protected in 'Car'
+}

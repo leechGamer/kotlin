ae84edc9717ea618d304bb77bf65c8dae5b8a285
g <ruddmsdl000@naver.com>
2023-06-25 18:43:00 +0900

    // 8.1.1 항상 정적으로 알려진 수신객체 타입을 기반으로 초출할 확장을 선택한다. 컴파일러는

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,11 +1,12 @@
-// 다형성
+// 8.1.1 항상 정적으로 알려진 수신객체 타입을 기반으로 초출할 확장을 선택한다. 컴파일러는
 open class Vehicle {
     open fun start() {
         println("I am moving")
     }
-    open fun stop() {
-        println("Stopped")
-    }
+}
+
+fun Vehicle.stop() {
+    println("Stopped")
 }
 
 class Car: Vehicle() {
@@ -14,18 +15,12 @@ class Car: Vehicle() {
     }
 }
 
-class Boat: Vehicle() {
-    override fun start() {
-        println("I am sailng")
-    }
-}
-
-fun startAndStop(vehicle: Vehicle) {
-    vehicle.start()
-    vehicle.stop()
+fun Car.stop() {
+    println("Stopped riding")
 }
 
 fun main() {
-    startAndStop(Car())
-    startAndStop(Boat())
+    val vehicle: Vehicle = Car()
+    vehicle.start()
+    vehicle.stop()
 }

44a3b2abda2b6f14507adcf747f9beac6445436d
g <ruddmsdl000@naver.com>
2023-06-25 22:14:45 +0900

    8.1.2 this 누출

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,14 +1,15 @@
-8.1.2 가상함수
+// 8.1.2 this 누출 
+// 상위 클래스가 현재의 인스턴스를 코드에 누출하는데,
+// 현재 인스턴스는 아직 초기화되지 않은 인스턴스의 상태에 의존할 수 있기 때문이다. 
 open class Person(
     val name: String,
     val age: Int
 ) {
-    open fun show() {
-        println("$name $age")
+    override fun toString(): String {
+        return "Person(name='$name', age=$age)"
     }
-
     init {
-        show()
+        println(this) // 잠재적인 위험요소
     }
 }
 
@@ -17,13 +18,12 @@ class Student(
     age: Int,
     val university: String
 ): Person(name, age) {
-    override fun show() {
-        println("$name $age (student at $university)")
+    override fun toString(): String {
+        return super.toString() + "(student at $university)"
     }
 }
 
 fun main() {
     Student("euan", 25, "MIT")
-    // euan 25 (student at null)
-
+    // Person(name='euan', age=25)(student at null)
 }

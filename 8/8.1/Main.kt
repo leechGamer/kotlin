7d7e0a7dbc75b3f59962440c543c1c249efee58e
g <ruddmsdl000@naver.com>
2023-06-26 11:26:06 +0900

    8.1.3 is 연산자

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,29 +1,13 @@
-// 8.1.2 this 누출 
-// 상위 클래스가 현재의 인스턴스를 코드에 누출하는데,
-// 현재 인스턴스는 아직 초기화되지 않은 인스턴스의 상태에 의존할 수 있기 때문이다. 
-open class Person(
-    val name: String,
-    val age: Int
-) {
-    override fun toString(): String {
-        return "Person(name='$name', age=$age)"
-    }
-    init {
-        println(this) // 잠재적인 위험요소
-    }
-}
+// 8.1.3 is 연산자
+val obj = arrayOf("1", 2, "3", 4)
 
-class Student(
-    name: String,
-    age: Int,
-    val university: String
-): Person(name, age) {
-    override fun toString(): String {
-        return super.toString() + "(student at $university)"
-    }
-}
+var sum = 0
 
 fun main() {
-    Student("euan", 25, "MIT")
-    // Person(name='euan', age=25)(student at null)
+    for (o in obj) {
+       if (o is Int) {
+           sum += o // obj 타입을 Int로 세분화 함
+       }
+    }
+    println(sum) // 6
 }

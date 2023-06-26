0905e5a21e28c751bf51d8125e811211a172661e
g <ruddmsdl000@naver.com>
2023-06-26 12:32:08 +0900

    // 8.1.4. 커스텀 동등성 연산

--- 8/8.1/Main.kt
+++ 8/8.1/Main.kt
@@ -1,8 +1,34 @@
-// 8.1.3 as, as?
+// 8.1.3 커스텀 동등성 연산
+// 프로퍼티는 같지만 두 인스턴스가 같은 객체로 간주되지 않는다.
+class Address(
+    val city: String,
+    val street: String,
+    val house: String,
+)
+
+open class Entity(
+    val name: String,
+    val address: Address
+)
+
+class Person(
+    name: String,
+    address: Address,
+    val age: Int,
+): Entity(name, address)
+
+class Organization(
+    name: String,
+    address: Address,
+    val manager: Person
+): Entity(name, address)
+
 fun main() {
-    val o: Any = 123
-    println((o as Int) + 1)
-    println((o as? Int)!! + 1)
-    println((o as? String ?: "").length)
-    println((o as String).length) // error: class cast exception
+    val address = arrayOf(
+        Address("London", "Lane", "8A"),
+        Address("New", "west", "11/B"),
+        Address("Sydney", "NOrteh road", "129"),
+    )
+
+    println(address.indexOf(Address("Sydney", "NOrteh road", "129"))) // -1
 }

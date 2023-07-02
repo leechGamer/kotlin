fa99c8cd932cc34d6aba029507339cb9b2d6736b
g <ruddmsdl000@naver.com>
2023-07-02 21:14:09 +0900

    8.2 추상멤버

--- 8/8.2/Main.kt
+++ 8/8.2/Main.kt
@@ -1,7 +1,35 @@
-abstract class Entity(val name: String)
+import kotlin.math.PI
 
-// 하위 클래스에서 위임 호출
-class Person(name: String, val age: Int): Entity(name)
+// 8.2.1 추상멤버: 타입, 파라미터, 반환 타입 등 함수나 프로퍼티의 기본적인 모습을 정의하지만 세부 구현은 생략한 멤버
 
-// error: cannot create an instance of an abstract class
-val entity = Entity("unknown")
+abstract class Shape {
+    abstract val width: Double
+    abstract val height: Double
+    abstract fun area(): Double
+}
+
+class Circle(val r: Double) : Shape() {
+    val diameter get() = 2 * r
+    override val width: Double
+        get() = diameter
+    override val height: Double
+        get() = diameter
+
+    override fun area() = PI * r * r
+}
+
+class Rectangle(
+    override val width: Double,
+    override val height: Double
+) : Shape() {
+    override fun area() = width * height
+}
+
+fun Shape.print() {
+    println("Bounds: $width* $height, area: ${area()}")
+}
+
+fun main() {
+    Circle(10.0).print()
+    Rectangle(3.0, 5.0).print()
+}

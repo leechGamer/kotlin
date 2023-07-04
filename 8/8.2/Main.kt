04e3c5b43a4437e83454d61f6c2f1db87f7a6492
g <ruddmsdl000@naver.com>
2023-07-04 09:05:53 +0900

    클래스 계층의 루트에 있는 추상 클래스는 전체 개념을 표현하고 하위클래스들은 특정 변종을 표현한다.

--- 8/8.2/Main.kt
+++ 8/8.2/Main.kt
@@ -1,27 +1,38 @@
+import java.lang.Exception
 import java.lang.NumberFormatException
 
-// 8.2.3 봉인된 클래스와 인터페이스
-
-enum class Result {
-    SUCCESS, ERROR
-}
-
-fun runComputation(): Result {
-    try {
-        val a = readLine()?.toInt() ?: return Result.ERROR
-        val b = readLine()?.toInt() ?: return Result.ERROR
-
-        println("Sum: ${a + b}")
-    } catch (e: NumberFormatException) {
-        return Result.ERROR
+// 8.2.3 클래스 계층을 활용해 모델링
+// 클래스 계층의 루트에 있는 추상 클래스는 전체 개념을 표현하고 하위클래스들은 특정 변종을 표현한다.
+abstract class Result {
+    class Success(val value: Any) : Result() {
+        fun showResult() {
+            println(value)
+        }
     }
-    return Result.SUCCESS
+
+    class Error(val message: String) : Result() {
+        fun throwException() {
+            throw Exception(message)
+        }
+    }
 }
+
+fun runComputaion(): Result {
+    try {
+        val a = readLine()?.toInt() ?: return Result.Error("Missing first argument")
+        val b = readLine()?.toInt() ?: return Result.Error("Missing second argument")
+        return Result.Success(a + b)
+    } catch (e: NumberFormatException) {
+        return Result.Error(e.message ?: "Invalid input")
+    }
+}
+
 
 fun main() {
-    val message = when(runComputation()) {
-        Result.SUCCESS -> "Completed successfully"
-        Result.ERROR -> "Error!"
+    val message = when (val result = runComputaion()) {
+        is Result.Success -> "Completed successfully : ${result.value}"
+        is Result.Error -> "Error: ${result.message}"
+        else -> return
     }
     println(message)
 }

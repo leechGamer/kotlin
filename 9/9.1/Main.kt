89859b6a9d41044d3073b3a47e54bacec753046f
g <ruddmsdl000@naver.com>
2023-07-10 21:59:00 +0900

    Update Main.kt

--- 9/9.1/Main.kt
+++ 9/9.1/Main.kt
@@ -1,25 +1,16 @@
-// 제네릭 클래스에 정의된 함수와 프로퍼티에서 .. 클래스의 타입 파라미터를 사용할 수 있다.
-// 프로퍼티나 함수에 타입 파라미터를 추가하면 ..프로퍼티나 함수 자체를 제네릭으로 만들 수 있다... 
 
-// 책 예제..
-fun <T> TreeNode<T>.addChildren(vararg data: T) {
-    data.forEach { addChild(it) }
-}
+// 상위 바운드로 Number를 선언하기
+fun <T: Number>TreeNode<T>.average(): Double {
+    var count = 0
+    var sum = 0.0
 
-fun <T> TreeNode<T>.walkDepthFirst(action: (T) -> Unit) {
-    children.forEach { it.walkDepthFirst(action) }
-    action(data)
-}
-
-val <T> TreeNode<T>.depth: Int
-    get() = (children.asSequence().map { it.depth }.maxOrNull() ?: 0) + 1
-
-fun main() {
-    val root = TreeNode("Hello").apply {
-        addChildren("world", "!!!")
+    walkDepthFirst {
+        count++
+        sum += it.toDouble()
     }
-
-    println(root.depth)
+    return sum / count
 }
+
+
 
 

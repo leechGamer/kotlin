b85afdb2dc749b491cf00586623543536ec85100
g <ruddmsdl000@naver.com>
2023-07-10 22:12:58 +0900

    Update Main.kt

--- 9/9.1/Main.kt
+++ 9/9.1/Main.kt
@@ -11,6 +11,12 @@ fun <T: Number>TreeNode<T>.average(): Double {
     return sum / count
 }
 
+// 타입 파라미터에 상위 바운드가 있으면 컴파일러는 이 타입 파라미터에 공급된 타입 인자의 타입이 상위 바운드의 하위 타입이지 검사한다.
+val intTree = TreeNode(1).apply {
+    addChild(2).addChild(3)
+    addChild(4).addChild(5)
+}
 
-
-
+fun main() {
+    println(intTree.average())
+}

07f86113c84711b44a609d53a6f261b9a86c20e9
g <ruddmsdl000@naver.com>
2023-07-10 22:46:11 +0900

    Update Main.kt

--- 9/9.1/Main.kt
+++ 9/9.1/Main.kt
@@ -1,10 +1,7 @@
-// 타입 파라미터 바운드로 타입 파라미터를 사용할 수도 있으며, 이것을 재귀적 타입 파라미터라고 말한다.
-fun <T: Comparable<T>> TreeNode<T>.maxNode(): TreeNode<T> {
-    val maxChilde = children.maxByOrNull { it.data } ?: return this
-
-    return if (data >= maxChilde.data) this else maxChilde
+// U가 T의 하위 타입이므로 트리 원소의 타입보다 더 일반적인 타입의 리스트를 인자로 받을 수 있다.
+fun <T, U: T> TreeNode<U>.toList(list: MutableList<T>) {
+    walkDepthFirst { list += it }
 }
-
 
 fun main() {
     // Double은 comparable<Double>의 하위 타입임
@@ -20,4 +17,20 @@ fun main() {
         addChildren("xyz", "def")
     }
     println(stringTree.maxNode().data)
+    
+    // 바운드가 자신보다 앞에 있는 타입 파라미터를 가리킬 수도 있다.
+    // 이런 바운드를 사용해 트리 원소를 가변 리스트에 추가하는 함수를 정의할 수 있다.
+    
+    // Int트리나 Double 트리에 있는 원소들은 Number 타입의 리스트에 추가할 수 있다. 
+    val list = ArrayList<Number>()
+    
+    TreeNode(1).apply { 
+        addChild(2)
+        addChild(3)
+    }.toList(list)
+    
+    TreeNode(1.0).apply { 
+        addChild(2.0)
+        addChild(3.0)
+    }.toList(list)
 }

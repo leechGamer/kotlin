1fb80dbb45313d7028cb8715417459b187888675
g <ruddmsdl000@naver.com>
2023-07-14 21:49:32 +0900

    Update Main.kt

--- 10/Main.kt
+++ 10/Main.kt
@@ -6,3 +6,14 @@ class MyTestCase {
         assert(1 + 1 == 2)
     }
 }
+
+
+//case 1
+//Expression annotations with retention other than SOURCE are prohibited
+@Target(AnnotationTarget.EXPRESSION)
+annotation class NeedToRefactor
+
+// case 1을 해결하려면 명시적으로 SOURCE 유지 시점을 지정해야 한다.
+@Target(AnnotationTarget.EXPRESSION)
+@Retention(AnnotationRetention.SOURCE)
+annotation class NeedToRefactor 

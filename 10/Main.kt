b4c95294643e8023bc79c806ecde49619974dc29
g <ruddmsdl000@naver.com>
2023-07-16 20:22:44 +0900

    Update Main.kt

--- 10/Main.kt
+++ 10/Main.kt
@@ -18,3 +18,17 @@ annotation class NeedToRefactor
 @Retention(AnnotationRetention.SOURCE)
 annotation class NeedToRefactor 
 
+// @Repeatable이 붙은 애너테이션을 같은 언어 요소에 두 번 이상 반복 적용할 수 있다.
+@Repeatable
+@Retention(AnnotationRetention.SOURCE)
+annotation class Author(val name: String)
+
+@Author("John")
+@Author("Harry")
+class Services
+
+
+// 기본적으로 애너테이션을 반복 적용할 수 없다
+@Deprecated("Deprecated")
+@Deprecated("Even more deprecated") // error: This annotation is not repeatable
+class OldClass
